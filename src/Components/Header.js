c885ffcd5b538f6e277bf8b1417efa2f823a22f0
EmersonJr <152315731+emersonjrdev@users.noreply.github.com>
2024-10-21 16:36:49 -0300

    Second commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Header() {
   return (
     <View style={styles.topo}>
-        <Text style={styles.tituloHeader}>InfoSass</Text>
+      <Text style={styles.tituloHeader}>InfoSass</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    topo: {
-        width: "100%",
-        height: 120,
-        backgroundColor: "gray",
-    },
-    tituloHeader: {
-        width: "100%",
-        textAlign: "center",
-        color: "white",
-        paddingTop: 60,
-        fontWeight: "bold",
-        fontSize: 20
-    }
-})
+  topo: {
+    width: '100%',
+    height: 120,
+    backgroundColor: '#3b5998', // Cor sólida de fundo
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#192f6a', // Borda inferior para dar estilo
+  },
+  tituloHeader: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)', // Sombra no texto
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+    textTransform: 'uppercase',
+    letterSpacing: 2,
+  },
+});
